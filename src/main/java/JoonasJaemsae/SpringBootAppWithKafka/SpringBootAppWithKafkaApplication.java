0e9f91a79f87ed1a36964c68aeedc5c8b561d1e6
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-15 11:17:27 +0300

    Initial commit

---
+++ src/main/java/JoonasJaemsae/SpringBootAppWithKafka/SpringBootAppWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package JoonasJaemsae.SpringBootAppWithKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JoonasJaemsae/SpringBootAppWithKafka/SpringBootAppWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package JoonasJaemsae.SpringBootAppWithKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
