10267eaf269d7399d33f4826adbf05a94f3e2312
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-16 14:30:09 +0300

    Add a producer for JSON messages

--- src/main/java/com/example/kafka/producer/JsonKafkaProducer.java
+++ src/main/java/com/example/kafka/producer/JsonKafkaProducer.java
@@ -1,2 +1,34 @@
-package com.example.kafka.producer;public class JsonKafkaProducer {
+package com.example.kafka.producer;
+
+import com.example.kafka.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data) {
+
+        LOGGER.info(String.format("JSON message sent: %s", data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "myJsonTopic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
 }
