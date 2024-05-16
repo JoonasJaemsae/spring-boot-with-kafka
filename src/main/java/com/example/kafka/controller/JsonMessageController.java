db76cfd55c7832ae594401496881d4d02a89f74b
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-16 14:29:38 +0300

    Add a JSON message controller

--- src/main/java/com/example/kafka/controller/JsonMessageController.java
+++ src/main/java/com/example/kafka/controller/JsonMessageController.java
@@ -1,2 +1,26 @@
-package com.example.kafka.controller;public class JsonMessageController {
+package com.example.kafka.controller;
+
+import com.example.kafka.payload.User;
+import com.example.kafka.producer.JsonKafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class JsonMessageController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    public JsonMessageController(JsonKafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish(@RequestBody User user) {
+        kafkaProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to Kafka topic.");
+    }
 }
