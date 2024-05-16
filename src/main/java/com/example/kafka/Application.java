39faefd00c455b91debe9738cd49a6130941c2d0
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-16 10:34:42 +0300

    Add main class

--- src/main/java/com/example/kafka/Application.java
+++ src/main/java/com/example/kafka/Application.java
@@ -1,2 +1,12 @@
-package com.example.kafka;public class Application {
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
