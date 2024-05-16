6f55408a437bdb34fb825929a7aabf05c78f227a
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-16 12:41:09 +0300

    Add rudimentary consumer with KafkaListener

---
+++ src/main/java/com/example/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.example.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    // Whenever a producer sends a message to the topic,
+    // consumer will receive and display it.
+    // groupId is the same as in application.properties
+    @KafkaListener(topics = "myTopic", groupId = "myGroup")
+    public void consume(String message) {
+        LOGGER.info(String.format("Message received by Consumer: %s", message));
+    }
+}
