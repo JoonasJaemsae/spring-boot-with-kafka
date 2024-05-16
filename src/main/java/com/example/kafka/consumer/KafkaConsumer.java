bfecb6584571d0a3114d98fb6db6c9dd5e779c98
Joonas Jämsä <jamsa.joonas@gmail.com>
2024-05-16 16:37:03 +0300

    Improve logs and comments

--- src/main/java/com/example/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafka/consumer/KafkaConsumer.java
@@ -10,12 +10,9 @@ public class KafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    // Whenever a producer sends a message to the topic,
-    // consumer will receive and display it.
-    // groupId is the same as in application.properties
     // If this Listener is sent a JSON object, it will result in an error while running the app.
     @KafkaListener(topics = "myTopic", groupId = "myGroup")
     public void consume(String message) {
-        LOGGER.info(String.format("Message received by Consumer: %s", message));
+        LOGGER.info(String.format("String message received by KafkaConsumer: %s", message));
     }
 }
